832d6436b89d3fbcc5fa57172954844b27050f8a
takoo <takoo@ubuntutakoo.myguest.virtualbox.org>
2023-09-22 07:58:31 +0300

    modified lab1/src/task.cpp

--- lab1/src/task.cpp
+++ lab1/src/task.cpp
@@ -1,13 +1,12 @@
-
-#include <iostream>
 #include <string>
+
+#include "task.hpp"
+
 
 #define BIN_STRING_SIZE (32)
 
-using namespace std;
-
-string IntToBinaryString(unsigned number) {
-    string binString(BIN_STRING_SIZE, '0');
+std::string IntToBinaryString(unsigned number) {
+    std::string binString(BIN_STRING_SIZE, '0');
     int binNumDigit = BIN_STRING_SIZE - 1;
     while (number >> 1) {
         binString[binNumDigit] = '0' + number % 2;
